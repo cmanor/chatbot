68ff156ed33f5166a08fdf59dd654149d2992c15
cmanor <101116379+cmanor@users.noreply.github.com>
2022-05-06 14:56:01 -0400

    mostly done. counter will only count if incorrect # entered first

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 //        greeting();
-        String name = repeatName();
-//        ageGuess();
-//        counting();
-//        knowledge();
-        story(name);
+//        String name = repeatName();
+//        int age = ageGuess();
+//        int count = counting();
+//        String answer = knowledge();
+//        story(name, age, count, answer);
+        counting();
 
     }
     public static void greeting() {
@@ -18,26 +19,27 @@ public class Main {
     public static String repeatName() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Uh...what did you say your name was again? ");
-        String name = scanner.nextLine();
+        String name = scanner.nextLine().trim();
         System.out.println("cool name bro");
         return name;
     }
-    public static void ageGuess() {
+    public static int ageGuess() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Wanna play a game? ");
         String response = scanner.nextLine();
         System.out.println("Rhetorical question. We're playing one.");
         System.out.println("I bet I can guess your age. If I guess it correctly, type \"yes\". By the way, we're doing this until I get it right.");
         while(true) {
-            System.out.println("Is it " + (int)(Math.random() * 100) + "?");
+            int guess = (int)(Math.random() * 100);
+            System.out.println("Is it " + guess + "?");
             response = scanner.nextLine();
             if (response.equals("yes")) {
                 System.out.println("Hell yeah, first try.");
-                break;
+                return guess;
             }
         }
     }
-    public static void counting() {
+    public static int counting() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Now enter a number greater than 1 and I'll count to it");
         int input = Integer.parseInt(scanner.nextLine());
@@ -49,9 +51,9 @@ public class Main {
                     System.out.println(i);
                 }
             }
-        }
+        }return input;
     }
-    public static void knowledge() {
+    public static String knowledge() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Time to test your programming knowledge.");
 
@@ -65,13 +67,17 @@ public class Main {
 
             if (answer.equals("B")){
                 System.out.println("Good job! You did it!");
-                break;
+                return answer;
             } else
                 System.out.println("wrong, try again");
         }
     }
-    public static void story(String nameResult) {
-        System.out.println("This is the story of " + nameResult);
+    public static void story(String nameResult, int ageResult, int countResult, String knowledgeResult) {
+        System.out.println("This is the story of " + nameResult + "."
+        + "They are definitely " + ageResult + " years old and totally didn't lie about it to stop me guessing their age anymore." + "\n"
+                + " Surprisingly, " + nameResult + " can count all the way to " + countResult + ", provided they are assisted by a computer." + "\n"
+                + " However, " + nameResult + "'s greatest feat is that they can eventually guess the correct answer \"" + knowledgeResult + "\"" + "\n"
+                + " of a multiple choice question (given enough tries).");
 
     }
     public static void dayOfWeek() {
